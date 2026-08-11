fc8efab2494fb5dd4140164468a93808ad094f2b
SanketWakde71405 <snktssw567@gmail.com>
2026-08-11 20:16:30 +0530

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int maxCount=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+            }else if(nums[i]==0){
+                if(count>maxCount){
+                    maxCount=count;
+                }
+                count=0;
+            }
+        }
+         if(count>maxCount){
+                    maxCount=count;
+                }
+
+            return maxCount;    
+
+        
+    }
+};
