60382e2d662520f115b9f688666cb26f4eb9a6c3
SanketWakde71405 <snktssw567@gmail.com>
2026-08-12 22:06:32 +0530

    Add: matrix-diagonal-sum - submission-0

---
+++ Data Structures & Algorithms/matrix-diagonal-sum/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum=0;
+        int j=mat.size()-1;
+        for(int i=0;i<mat.size();i++){
+            sum+=mat[i][i];
+
+            if(i!=j){
+             sum+=mat[i][j];
+            }
+           j--;
+        }
+
+        return sum;
+    }
+};
